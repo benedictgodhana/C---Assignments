abab83b1b01c1a13106b0a530a59828f606b5cb9
benedictgodhana <benedictgodhana7@gmail.com>
2024-06-09 17:40:25 +0300

    updates

--- strathmore.cpp
+++ strathmore.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 using namespace std;
-int main(){
 
-    //print title of the vision statement
-    cout<<"Strathmore University Vision\n";
+// Function prototypes
+void line();
+void message();
+void header();
+int main() {
+    // Print title of the vision statement
 
-    //print line of dots to visually separate the title from the vision statement
-    cout<<"...........................\n\n";
-    //print  the vision statement
-    cout<<"To become  a leading out-come driven entrepreneurial research \n";
-    cout<<"University by translating our excellence into major contribution \n";
-    cout<<"to culture, economic well-being and quality of life\n";
+    header();
+    // Print line of dots to visually separate the title from the vision statement
+    line();
+
+    // Print the vision statement
+    message();
+
+    return 0;
+}
+
+
+void header(){
+        cout << "Strathmore University Vision\n";
+}
+
+// Function to print a line of dots
+void line() {
+    cout << "...........................\n\n";
+}
+
+// Function to print the vision statement
+void message() {
+    cout << "To become a leading outcome-driven entrepreneurial research \n";
+    cout << "University by translating our excellence into major contribution \n";
+    cout << "to culture, economic well-being and quality of life\n";
 }
